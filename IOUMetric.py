eb8776b5398d709f5311d1bcb466aa54e8dfb0b6
Dingxueqn <dingxueqn@163.com>
2025-08-12 14:42:25 +0800

    Update IOUMetric.py

--- IOUMetric.py
+++ IOUMetric.py
@@ -11,16 +11,13 @@ class IOUMetric:
         self.hist = np.zeros((num_classes, num_classes))
 
     def _fast_hist(self, label_pred, label_true):
-        # 找出标签中需要计算的类别,去掉了背景
         mask = (label_true >= 0) & (label_true < self.num_classes)
-        # # np.bincount计算了从0到n**2-1这n**2个数中每个数出现的次数，返回值形状(n, n)
         hist = np.bincount(
             self.num_classes * label_true[mask].astype(int) +
             label_pred[mask], minlength=self.num_classes ** 2).reshape(self.num_classes, self.num_classes)
         return hist
 
-    # 输入：预测值和真实值
-    # 语义分割的任务是为每个像素点分配一个label
+
     def evaluate(self, predictions, gts):
 
         for lp, lt in zip(predictions, gts):
