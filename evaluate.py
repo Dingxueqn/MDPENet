c9ccad993704f8df3b642a89a254812ab11642fa
Dingxueqn <dingxueqn@163.com>
2025-08-12 14:46:20 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,13 +3,7 @@ import os
 import json
 
 
-# 获取混淆矩阵四个值（以道路提取为例，道路区域【255,255,255】，背景区域【0,0，0】）
-# TP：被模型预测为正类的正样本(预测道路且标签道路)
-# TN：被模型预测为负类的负样本（预测背景且真实背景）
-# FP：被模型预测为正类的负样本（预测道路但真实背景）
-# FN：被模型预测为负类的正样本（预测背景但真实道路）
 def get_vaslue(predict_folders_path, label_folders_path):
-    # 加载文件夹
     predict_folders = os.listdir(predict_folders_path)
     label_folders = os.listdir(label_folders_path)
     TP = 0
@@ -17,10 +11,8 @@ def get_vaslue(predict_folders_path, label_folders_path):
     FP = 0
     FN = 0
     for folder in predict_folders:
-        # 获取图片路径
         predict_folder_path = os.path.join(predict_folders_path, folder)
         label_folder_path = os.path.join(label_folders_path, folder)
-        # 加载图像并赋值四通道
         predict = Image.open(predict_folder_path)
         predict = predict.convert('RGBA')
         label = Image.open(label_folder_path)
@@ -44,7 +36,6 @@ def get_vaslue(predict_folders_path, label_folders_path):
     return float(TP), float(TN), float(FP), float(FN)
 
 
-# list转存txt
 def list2txt(list, save_path, txt_name):
     with open(save_path + r'/' + txt_name, 'w') as f:
         json.dump(list, f)
@@ -52,11 +43,8 @@ def list2txt(list, save_path, txt_name):
 
 def evoluation(TP, TN, FP, FN):
     evo = []
-    # 准确率
     accuracy = (TP + TN) / (TP + TN + FP + FN)
-    # 精准率
     precision = TP / (TP + FP)
-    # 召回率
     recall = TP / (TP + FN)
     # miou
     miou = (TP / (TP + FP + FN) + TN / (TN + FN + FP)) / 2
@@ -68,8 +56,8 @@ def evoluation(TP, TN, FP, FN):
 
 
 if __name__ == '__main__':
-    label_path = "output/colon/gt"  # 标签的文件夹位置
-    predict_path = "output/colon/pred"  # 预测结果的文件夹位置
+    label_path = "output/colon/gt"  
+    predict_path = "output/colon/pred" 
     TP, TN, FP, FN = get_vaslue(predict_path, label_path)
     evoluation(TP, TN, FP, FN)
 
